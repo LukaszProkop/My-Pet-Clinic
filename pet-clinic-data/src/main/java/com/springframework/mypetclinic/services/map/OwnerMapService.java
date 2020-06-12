14fa5c2297de9945d41a0c71a85de48f5db81a1c
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-06-13 01:41:58 +0200

    Updated profiles for JPA service

--- pet-clinic-data/src/main/java/com/springframework/mypetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/mypetclinic/services/map/OwnerMapService.java
@@ -5,11 +5,13 @@ import com.springframework.mypetclinic.model.Pet;
 import com.springframework.mypetclinic.services.OwnerService;
 import com.springframework.mypetclinic.services.PetService;
 import com.springframework.mypetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
--- pet-clinic-data/src/main/java/com/springframework/mypetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/mypetclinic/services/map/PetMapService.java
@@ -2,11 +2,13 @@ package com.springframework.mypetclinic.services.map;
 
 import com.springframework.mypetclinic.model.Pet;
 import com.springframework.mypetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
